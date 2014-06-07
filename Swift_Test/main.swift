5c241bc757694f24bc86aab40c8616bc199b4c06
Tsuki <tsuki@Tsuki-MacB.local>
2014-06-08 03:16:14 +0800

    Initial Commit

---
+++ Swift_Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Test
+//
+//  Created by  Tsuki on 14-6-8.
+//  Copyright (c) 2014年  Tsuki. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
