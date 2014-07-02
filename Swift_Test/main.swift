6bbfde19f04280cf5e95aeae4cada4a4463c0f9f
chenfeifei <chen3286822@yeah.net>
2014-07-03 00:10:39 +0800

    some test codes

--- Swift_Test/main.swift
+++ Swift_Test/main.swift
@@ -10,7 +10,22 @@ import Foundation
 
 println("Hello, World!")
 
-var test : Double = 3.0
+var test : Double = 3.5
 let test2 = 5
 var test3 : String = "hello world"
 var test4 : Bool = false
+let u16 = UInt16.max
+println(u16)
+//let u8:UInt8 = UInt8(u16)
+//(u8)
+println(Int(test))
+typealias MyInt = Int64
+var maxInt64 = MyInt.max
+println(maxInt64)
+var tuple1 :(String,Int) = ("陈飞飞",24)
+println(tuple1)
+println(tuple1.0)
+let (name,age) = tuple1
+let (name2,_) = tuple1
+let tuple2 = (first:200,second:300)
+
